5d3584007cfdb357fdeb094e20ca5da23c42ec87
Вини <yvinivitin@gmail.com>
2020-10-05 17:34:33 +0300

    Активация стр; Заполнение формы и валидация

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const mapElement = document.querySelector(`.map`);
-const pinTemplateElement = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
-const cardTemplateElement = document.querySelector(`#card`).content.querySelector(`.popup`);
-const mapPinsElement = document.querySelector(`.map__pins`);
-const filtersContainerElement = mapElement.querySelector(`.map__filters-container`);
 const ROOMS_TYPE = [`palace`, `flat`, `house`, `bungalow`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
@@ -12,19 +7,48 @@ const NUMBERS_AVATAR = [`01`, `02`, `03`, `04`, `05`, `06`, `07`, `08`];
 const PIN_WIDTH = 40 / 2;
 const PIN_HEIGHT = 40;
 const MAX_OFFERS = 8;
+const MAP_MAIN_PIN_WIDTH = 62 / 2;
+const MAP_MAIN_PIN_HEIGHT = 84;
+const mapElement = document.querySelector(`.map`);
+const mapFiltersElement = document.querySelector(`.map__filters`);
+const mapPinMainElement = document.querySelector(`.map__pin--main`);
+const pinTemplateElement = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+const cardTemplateElement = document.querySelector(`#card`).content.querySelector(`.popup`);
+const mapPinsElement = document.querySelector(`.map__pins`);
+const filtersContainerElement = mapElement.querySelector(`.map__filters-container`);
+const formAddressElement = document.querySelector(`#address`);
+const formTypeElement = document.querySelector(`#type`);
+const formPriceElement = document.querySelector(`#price`);
+const adFormElement = document.querySelector(`.ad-form`);
+const formAvatarElement = document.querySelector(`#avatar`);
+const formTitleElement = document.querySelector(`#title`);
+const formTimeinElement = document.querySelector(`#timein`);
+const formTimeoutElement = document.querySelector(`#timeout`);
+const formRoomCapacityElement = document.querySelector(`#capacity`);
+const formRoomNumberElement = document.querySelector(`#room_number`);
+const formDescriptionElement = document.querySelector(`#description`);
+const formImagesElement = document.querySelector(`#images`);
+const formSubmitButtonElement = document.querySelector(`.ad-form__submit`);
+const mapFeaturesElement = document.querySelector(`.map__features`);
+const formAllFieldsetsElements = adFormElement.querySelectorAll(`.ad-form__element`);
+const formHeaderFieldsetElement = adFormElement.querySelector(`.ad-form-header`);
+const mapFiltersAllElements = mapFiltersElement.querySelectorAll(`.map__filter`);
+const formAddressX = parseInt(mapPinMainElement.style.left, 10) + MAP_MAIN_PIN_WIDTH;
+const formAddressY = parseInt(mapPinMainElement.style.top, 10) + MAP_MAIN_PIN_HEIGHT;
+const elementsDisableEnable = [];
 const pins = [];
 
-const getRandomNumber = function (max) {
+function getRandomNumber(max) {
   return Math.floor(Math.random() * Math.floor(max));
-};
+}
 
-const getRandomNumberFromRange = function (min, max) {
+function getRandomNumberFromRange(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
-const getRandomItemsFromArray = function (arr) {
+function getRandomItemsFromArray(arr) {
   const myArr = arr.slice(0);
   const randomLength = getRandomNumber(myArr.length + 1);
 
@@ -33,9 +57,9 @@ const getRandomItemsFromArray = function (arr) {
   }
 
   return myArr;
-};
+}
 
-const createOffer = function () {
+function createOffer() {
   const location = {
     x: getRandomNumberFromRange(30, 1170),
     y: getRandomNumberFromRange(160, 600),
@@ -60,9 +84,9 @@ const createOffer = function () {
     },
     location,
   };
-};
+}
 
-const renderPin = function (ad) {
+function renderPin(ad) {
   const element = pinTemplateElement.cloneNode(true);
   const img = element.querySelector(`img`);
 
@@ -72,9 +96,9 @@ const renderPin = function (ad) {
   img.alt = ad.offer.title;
 
   return element;
-};
+}
 
-const renderCard = function (ad) {
+function renderCard(ad) {
   const element = cardTemplateElement.cloneNode(true);
   const titleElement = element.querySelector(`.popup__title`);
   const addressElement = element.querySelector(`.popup__text--address`);
@@ -116,9 +140,9 @@ const renderCard = function (ad) {
   });
 
   return element;
-};
+}
 
-const renderPins = function (arr) {
+function renderPins(arr) {
   const fragment = document.createDocumentFragment();
 
   arr.forEach(function (element) {
@@ -126,9 +150,9 @@ const renderPins = function (arr) {
   });
 
   return fragment;
-};
+}
 
-const renderCards = function (arr) {
+function renderCards(arr) {
   const fragment = document.createDocumentFragment();
 
   arr.forEach(function (element) {
@@ -136,7 +160,76 @@ const renderCards = function (arr) {
   });
 
   return fragment;
-};
+}
+
+function disableEverything(disable) {
+  disable.setAttribute(`disabled`, `disabled`);
+}
+
+function enableEverything(enable) {
+  enable.removeAttribute(`disabled`, `disabled`);
+}
+
+function changeTwoElementValue(change, changer) {
+  change.value = changer.value;
+}
+
+function onRoomNumberChange() {
+  if (formRoomCapacityElement.value === '0' && formRoomNumberElement.value !== '100') {
+    formRoomNumberElement.setCustomValidity('Только 100 комнат!');
+  } else if (formRoomNumberElement.value === '100' && formRoomCapacityElement.value !== '0') {
+    formRoomCapacityElement.setCustomValidity('Не для гостей');
+  } else if (formRoomNumberElement.value < formRoomCapacityElement.value) {
+    formRoomCapacityElement.setCustomValidity('Понижай или проиграешь.');
+    formRoomNumberElement.setCustomValidity('');
+  } else {
+    formRoomCapacityElement.setCustomValidity('');
+    formRoomNumberElement.setCustomValidity('');
+  }
+}
+
+function onFormTypeChange() {
+  switch (formTypeElement.value) {
+    case `bungalow`:
+      formPriceElement.setAttribute(`placeholder`, 0);
+      formPriceElement.setAttribute(`min`, 0);
+      break;
+    case `flat`:
+      formPriceElement.setAttribute(`placeholder`, 1000);
+      formPriceElement.setAttribute(`min`, 1000);
+      break;
+    case `house`:
+      formPriceElement.setAttribute(`placeholder`, 5000);
+      formPriceElement.setAttribute(`min`, 5000);
+      break;
+    case `palace`:
+      formPriceElement.setAttribute(`placeholder`, 10000);
+      formPriceElement.setAttribute(`min`, 10000);
+      break;
+  }
+}
+
+function onRoomCapacityChange() {
+  if (formRoomCapacityElement.value === '0' && formRoomNumberElement.value !== '100') {
+    formRoomNumberElement.setCustomValidity('Только 100 комнат!');
+  } else if (formRoomNumberElement.value === '100' && formRoomCapacityElement.value !== '0') {
+    formRoomCapacityElement.setCustomValidity('Не для гостей');
+  } else if (formRoomNumberElement.value < formRoomCapacityElement.value) {
+    formRoomCapacityElement.setCustomValidity('Понижай или проиграешь.');
+    formRoomNumberElement.setCustomValidity('');
+  } else {
+    formRoomCapacityElement.setCustomValidity('');
+    formRoomNumberElement.setCustomValidity('');
+  }
+}
+
+function onTimeinChange() {
+  changeTwoElementValue(formTimeoutElement, formTimeinElement);
+}
+
+function onTimeoutChange() {
+  changeTwoElementValue(formTimeinElement, formTimeoutElement);
+}
 
 for (let i = 0; i < MAX_OFFERS; i++) {
   pins.push(createOffer());
@@ -145,41 +238,51 @@ for (let i = 0; i < MAX_OFFERS; i++) {
 const pinsFragment = renderPins(pins);
 const cardsFragment = renderCards(pins);
 
-mapPinsElement.appendChild(pinsFragment);
+// mapPinsElement.appendChild(pinsFragment);
+// mapElement.insertBefore(cardsFragment, filtersContainerElement);
 
-const mapPinMainElement = document.querySelector(`.map__pin--main`);
+elementsDisableEnable.push(mapFeaturesElement, formHeaderFieldsetElement);
+
+mapFiltersAllElements.forEach(function (element) {
+  elementsDisableEnable.push(element);
+});
+
+formAllFieldsetsElements.forEach(function (element) {
+  elementsDisableEnable.push(element);
+});
+
+elementsDisableEnable.forEach(function (element) {
+  disableEverything(element);
+});
 
 mapPinMainElement.addEventListener(`mousedown`, function (evt) {
   if (evt.button === 0) {
     mapElement.classList.remove(`map--faded`);
+    adFormElement.classList.remove(`ad-form--disabled`);
+    elementsDisableEnable.forEach(function (element) {
+      enableEverything(element);
+    });
+    formAddressElement.setAttribute(`placeholder`, formAddressX + `, ` + formAddressY);
+    formTimeinElement.addEventListener(`change`, onTimeinChange);
+    formTimeoutElement.addEventListener(`change`, onTimeoutChange);
+    formTypeElement.addEventListener(`change`, onFormTypeChange);
+    formRoomNumberElement.addEventListener(`change`, onRoomNumberChange);
+    formRoomCapacityElement.addEventListener(`change`, onRoomCapacityChange);
   }
-  inputAddressElement.setAttribute(`placeholder`, `300, 200`);
 });
 
 mapPinMainElement.addEventListener(`keydown`, function (evt) {
   if (evt.key === `Enter`) {
     mapElement.classList.remove(`map--faded`);
-  }
-
-});
-
-
-
-const inputAddressElement = document.querySelector(`#address`);
-const inputTypeElement = document.querySelector(`#type`);
-const inputPriceElement = document.querySelector(`#price`);
-const adFormElement = document.querySelector(`.ad-form`);
-
-adFormElement.classList.remove(`ad-form--disabled`);
-
-inputTypeElement.addEventListener(`change`, function () {
-  console.log(inputTypeElement.value);
-  if (inputTypeElement.value === 'bungalow') {
-    inputPriceElement.setAttribute(`placeholder`, 0);
-    inputPriceElement.setAttribute(`min`, 0);
+    adFormElement.classList.remove(`ad-form--disabled`);
+    elementsDisableEnable.forEach(function (element) {
+      enableEverything(element);
+    });
+    formAddressElement.setAttribute(`placeholder`, formAddressX + `, ` + formAddressY);
+    formTimeinElement.addEventListener(`change`, onTimeinChange);
+    formTimeoutElement.addEventListener(`change`, onTimeoutChange);
+    formTypeElement.addEventListener(`change`, onFormTypeChange);
+    formRoomNumberElement.addEventListener(`change`, onRoomNumberChange);
+    formRoomCapacityElement.addEventListener(`change`, onRoomCapacityChange);
   }
 });
-
-
-
-// mapElement.insertBefore(cardsFragment, filtersContainerElement);
