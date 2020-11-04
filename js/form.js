dbb2a15e3ea377e6da727edae9997f561f213b32
Вини <yvinivitin@gmail.com>
2020-11-04 21:44:31 +0200

    Удалил обработчики и повторяю код

--- js/form.js
+++ js/form.js
@@ -6,7 +6,7 @@
   const adFormElement = document.querySelector(`.ad-form`);
   const formResetButtonElement = adFormElement.querySelector(`.ad-form__reset`);
 
-  formResetButtonElement.addEventListener(`click`, function () {
+  function turnUnactiveState() {
     adFormElement.reset();
     mapFiltersElement.reset();
     window.formValidity.onFormTypeChange();
@@ -19,21 +19,14 @@
     window.resetPage.removePinsOnMap();
     window.resetPage.setMainPinCenter();
     window.formValidity.formAddressElement.setAttribute(`value`, window.dragging.formAddressX + `, ` + window.dragging.formAddressY);
+  }
+
+  formResetButtonElement.addEventListener(`click`, function () {
+    turnUnactiveState();
   });
 
   function successFormHandler() {
-    adFormElement.reset();
-    mapFiltersElement.reset();
-    window.formValidity.onFormTypeChange();
-    mapElement.classList.add(`map--faded`);
-    adFormElement.classList.add(`ad-form--disabled`);
-    window.resetPage.elementsDisableEnable.forEach(function (element) {
-      window.util.toggleDisabled(element, true);
-    });
-    window.resetPage.removeCardOnMap();
-    window.resetPage.removePinsOnMap();
-    window.resetPage.setMainPinCenter();
-    window.formValidity.formAddressElement.setAttribute(`value`, window.dragging.formAddressX + `, ` + window.dragging.formAddressY);
+    turnUnactiveState();
   }
 
   adFormElement.addEventListener(`submit`, function (evt) {
